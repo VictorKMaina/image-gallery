6edfc4f6749c53a88a7777c9813a93080e560f2f
Victor Maina <contact@victormaina.com>
2020-11-17 16:45:21 +0300

    add images to index.html

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.http import Http404
 
 
 # Create your views here.
 def index(request):
-    return render(request, "gallery/index.html", )
+    images = Image.objects.all()
+    ctx = {"images":images}
+    return render(request, "gallery/index.html", ctx)
