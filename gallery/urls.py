6f4d289c53369cde67691aaeaedb2a0c82b503b6
Victor Maina <contact@victormaina.com>
2020-11-17 23:12:46 +0300

    added view function and url for location

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "home"),
-    path("search/", views.search_results, name="search_results")
+    path("search/", views.search_results, name="search_results"),
+    path("location/<int:location_id>", views.location, name = "location"),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -5,8 +5,11 @@ from django.http import Http404
 # Create your views here.
 def index(request):
     images = Image.objects.all()
-    ctx = {"images":images}
-    return render(request, "gallery/index.html", ctx)
+    if len(images) > 0:
+        ctx = {"images":images}
+        return render(request, "gallery/index.html", ctx)
+    else:
+        message = {""}
 
 def search_results(request):
     if "category" in request.GET and request.GET["category"]:
@@ -21,3 +24,19 @@ def search_results(request):
         message = "You have not searched for any item."
         ctx = {"message":message}
         return render(request, "gallery/search_results.html", ctx)
+
+def location(request, location_id):
+    try:
+        location = Location.objects.get(id = location_id).location
+        images = Image.filter_by_location(location)
+
+        if len(images) > 0:
+            message = f"Images from {location}"
+            ctx = {"images":images, "message":message}
+            return render(request, "gallery/search_results.html", ctx)
+        else:
+            message = f"No images from {location}"
+            ctx = {"message":message}
+            return render(request, "gallery/search_results.html", ctx)
+    except:
+        raise Http404("Page does not exist.")
