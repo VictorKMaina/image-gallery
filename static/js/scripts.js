a03a71a62db27914fa29e98bfd1d079f11eacebf
Victor Maina <contact@victormaina.com>
2020-11-17 19:13:02 +0300

    add functionality for copying image url

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
     $(".grid-image").click(function(){
         $("#imageModal").modal('toggle')
     })
+
+    $(".grid-image").click(function(){
+        $(".alert#copied").hide()
+        $("button#copy-link").click(function(){
+            let imageLink = $("input#image-link");
+            imageLink.select();
+            document.execCommand("copy");
+            $(".alert#copied").show()
+        })
+    })
 })
